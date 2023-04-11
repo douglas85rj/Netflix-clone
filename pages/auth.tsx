8d4860f61149ac7054dd60f5af8cc521440376e3
douglas85rj <douglas85rj@gmail.com>
2023-04-11 20:56:43 -0300

    auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import Input from "@/components/input";
 import { useCallback, useState } from "react";
+import axios from "axios";
 
 const Auth = () => {
 
@@ -15,8 +16,17 @@ const Auth = () => {
     }, []);
 
     const register = useCallback (async() => {
-
-    }, []);
+        try {
+            await axios.post('api/register',{
+                email,
+                name,
+                password
+            });
+        }catch(error) {
+            console.log(error);
+        };
+        
+    }, [email,name,password]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -60,7 +70,7 @@ const Auth = () => {
                             />
                         </div>
 
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
